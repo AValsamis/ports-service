ebb5d4b8329b63b7bf3b93725a590ca1039aac3d
Angelos Valsamis <angelos@sliide.com>
2023-07-12 14:17:50 +0300

    include main command and dockerfile

---
+++ cmd/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"ports-service/internal/ports/repository"
+	"ports-service/internal/ports/service"
+)
+
+func main() {
+	terminateCh := make(chan os.Signal, 1)
+	signal.Notify(terminateCh, syscall.SIGINT, syscall.SIGTERM)
+
+	repo := repository.NewPortRepository()
+	srv := service.NewPortService(repo)
+
+	// Load ports from the PORTS_JSON_PATH file
+	filePath := os.Getenv("PORTS_JSON_PATH")
+	if filePath == "" {
+		fmt.Println("PORTS_JSON_PATH environment variable not set")
+		os.Exit(1)
+	}
+
+	file, err := os.Open(filePath)
+	if err != nil {
+		fmt.Printf("Failed to open file: %v\n", err)
+		os.Exit(1)
+	}
+	defer file.Close()
+
+	err = srv.LoadPorts(file, terminateCh)
+	if err != nil {
+		fmt.Printf("Failed to load ports from file: %v\n", err)
+		os.Exit(1)
+	}
+
+	fmt.Printf("Ports imported: %d\n", repo.GetPortsLength())
+}
